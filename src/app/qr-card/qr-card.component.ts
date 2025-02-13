7be6f65ccbbf9525b5af95b126dfb2d74ff6d825
riftvalleydev <dev@riftvalley.com>
2025-02-13 07:49:32 +0200

    added qr-card component

---
+++ src/app/qr-card/qr-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './qr-card.component.html',
+  styleUrl: './qr-card.component.css'
+})
+export class QrCardComponent {
+
+}
